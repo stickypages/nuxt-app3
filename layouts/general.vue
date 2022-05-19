469f41a443e7ebd972113cee60be9b4596fe47cb
Justin <justin@stickypages.ca>
2022-05-19 12:09:11 -0700

    Initial commit

---
+++ layouts/general.vue
@@ -0,0 +1,19 @@
+<script setup>
+const route = useRoute()
+const title = route.meta.title
+
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Title>{{ title }}</Title>
+      <Style type="text/css" children="body { background-color: light-grey; }" />
+    </Head>
+
+    General Template
+
+    <slot />
+
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+definePageMeta({
+  layout: 'general',
+  title: 'Some Page'
+})
+
+</script>
+
+<template>
+  <div>
+    Hello
+    <test />
+    <img src="/img/MyShout-Blue.png" />
+  </div>
+</template>
